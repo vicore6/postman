ddc3ebf16af3aed84f37542320e745e2f214ded6
vicore6 <vicore6@gmail.com>
2022-04-06 21:04:51 +0300

    v1

---
+++ src/test/java/ru/netology/MobileBankApiTestPost.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+class MobileBankApiTestPost {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()// Предусловия
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("привет")// отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()// Выполняемые действия
+                .post("/post")
+                .then()// Проверки
+                .statusCode(200)
+                .body("data", equalTo("привет"))
+        ;
+    }
+}
